a49010a220b48c9bd4843af997e37c0350d1972f
Daniel Merritt <daniel.mafou@gmail.com>
2023-07-07 14:36:03 -0400

    add function

--- main.py
+++ main.py
@@ -3,12 +3,26 @@ import tkinter as tk
 calculation = ""
 
 def add_to_calculation(symbol):
-    pass
+  global calculation
+  calculation += str(symbol)
+  text_result.delete(1.0, "end")
+  text_result.insert(1.0, calculation)
 
 def evaluate_calculation():
+  global calculation
+  try:
+    result = str(eval(calculation))
+    calculation = ""
+  except:
     pass
 
 def clear_field():
-    pass
+  pass
 
 root = tk.Tk()
+root.geometry("300x275")
+
+text_result = tk.Text(root, height=2, width=16, font=("Arial", 24))
+text_result.grid(columnspan=5)
+
+root.mainloop()
