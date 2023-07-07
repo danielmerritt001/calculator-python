c2a2825cda7a3f34ff8883b668675207c4573ac8
Daniel Merritt <daniel.mafou@gmail.com>
2023-07-07 15:10:16 -0400

    number buttons

--- main.py
+++ main.py
@@ -11,13 +11,17 @@ def add_to_calculation(symbol):
 def evaluate_calculation():
   global calculation
   try:
-    result = str(eval(calculation))
-    calculation = ""
+    calculation = str(eval(calculation))
+    text_result.delete(1.0, "end")
+    text_result.insert(1.0, calculation)
   except:
-    pass
+    clear_field()
+    text_result.insert(1.0, "Error")
 
 def clear_field():
-  pass
+  global calculation
+  calculation = ''
+  text_result.delete(1.0, "end")
 
 root = tk.Tk()
 root.geometry("300x275")
@@ -25,4 +29,24 @@ root.geometry("300x275")
 text_result = tk.Text(root, height=2, width=16, font=("Arial", 24))
 text_result.grid(columnspan=5)
 
+btn_1 = tk.Button(root, text='1', command=lambda: add_to_calculation(1), width=5, font=("Arial", 15))
+btn_1.grid(row=2, column=1)
+btn_2 = tk.Button(root, text='2', command=lambda: add_to_calculation(2), width=5, font=("Arial", 15))
+btn_2.grid(row=2, column=2)
+btn_3 = tk.Button(root, text='3', command=lambda: add_to_calculation(3), width=5, font=("Arial", 15))
+btn_3.grid(row=2, column=3)
+btn_4 = tk.Button(root, text='4', command=lambda: add_to_calculation(4), width=5, font=("Arial", 15))
+btn_4.grid(row=3, column=1)
+btn_5 = tk.Button(root, text='5', command=lambda: add_to_calculation(5), width=5, font=("Arial", 15))
+btn_5.grid(row=3, column=2)
+btn_6 = tk.Button(root, text='6', command=lambda: add_to_calculation(6), width=5, font=("Arial", 15))
+btn_6.grid(row=3, column=3)
+btn_7 = tk.Button(root, text='7', command=lambda: add_to_calculation(7), width=5, font=("Arial", 15))
+btn_7.grid(row=4, column=1)
+btn_8 = tk.Button(root, text='8', command=lambda: add_to_calculation(8), width=5, font=("Arial", 15))
+btn_8.grid(row=4, column=2)
+btn_9 = tk.Button(root, text='9', command=lambda: add_to_calculation(9), width=5, font=("Arial", 15))
+btn_9.grid(row=4, column=3)
+btn_0 = tk.Button(root, text='0', command=lambda: add_to_calculation(0), width=5, font=("Arial", 15))
+btn_0.grid(row=5, column=2)
 root.mainloop()
